1a25eac7b66063c16d84b0908e72e34776413eef
Carl Schubert & Frank DelPidio <pair+carlschubert.fdel15@devbootcamp.com>
2014-12-12 18:58:34 -0500

    add patial implementation of game loop

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -1,13 +1,54 @@
-get '/game/new' do
+post '/games' do
   @answer = City.all.shuffle.first
-
-  erb :'/game/question'
+  game = Game.create(score:nil, user_id: session[:user_id])
+  redirect "/games/#{game.id}"
 end
 
-post '/game/answer' do
-  Question.create(guess: params[:question][:guess], answer_id: params[:question][:answer],
+get '/games/:id' do |id|
+
+  # Grab a question from the database
+  # put it in the erb as an instance var
+  # At this point correct is nil cos the user hasn't answered
+  # Then in post '/game/answer' you'd look for this question
+  # and update it
+=begin
+   In the erb you'd just have question_id in a hidden
+   and an input field for the user to answer in.
+    post /game/answer would do
+      q = Question.find(params[:question_id])
+      guess = params[:guess]
+      q.correct = (guess == q.city.name)
+      q.guess = guess
+      q.save
+  @question
+=end
 
 
-  @city = City.find(@question.city_id).name
-  if params[:question][:guess]
+  previous_wrong = Question.find_by(correct: false, game_id: id)
+  if previous_wrong > 2
+    @game_over = "Game over"
+  else
+    @game = Game.find(id);
+    @game.questions
+  end
+  erb :"/game/question"
 end
+
+ post '/game/answer' do
+   q = Question.create(params[:question])
+   correct_city = q.city
+   q.correct = (q.guess == q.city.name)
+   q.save
+   redirect "/games/#{q.game_id}"
+ end
+
+
+
+
+
+
+
+
+#   @city = City.find(@question.city_id).name
+#   if params[:question][:guess]
+# end

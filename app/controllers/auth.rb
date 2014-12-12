45f036114925346da076ca6a7821b524c08c9f31
Carl Schubert & Frank DelPidio <pair+carlschubert.fdel15@devbootcamp.com>
2014-12-12 16:10:40 -0500

    add login logic

---
+++ app/controllers/auth.rb
@@ -0,0 +1,15 @@
+get '/login' do
+  erb :'auth/login'
+end
+
+post '/login' do
+  user = User.find_by(name: params[:user][:name])
+
+  if user && user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/login'
+  end
+end
+

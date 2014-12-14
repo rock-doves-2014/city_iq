a3bb348627fad06b65c008c7c7939f0b8acef983
Frank DelPidio & Ricky Thomas <pair+fdel15.ricky-thomas@devbootcamp.com>
2014-12-14 11:44:32 -0500

    created current_image and current_game helpers using session

--- app/helpers/auth.rb
+++ app/helpers/auth.rb
@@ -6,4 +6,12 @@ helpers do
     @user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_game
+    @game ||= Game.find(session[:game_id] if session[:game_id])
+  end
+
+  def current_image
+    @image ||= Image.find(session[:image_id] if session[:image_id])
+  end
+
 end
